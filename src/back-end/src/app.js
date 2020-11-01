aa2ed9f1675247aa2e2793d64c9fba8480f7388d
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-01 01:48:38 +0100

    Send location to backend

--- src/back-end/src/app.js
+++ src/back-end/src/app.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+var cors = require('cors')
+const port = 3000;
+const jsonParser = bodyParser.json();
+app.use(cors());
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hey there!');
+});
+
+app.post('/', jsonParser, function (req, res) {
+  console.log(req.body);
+});
