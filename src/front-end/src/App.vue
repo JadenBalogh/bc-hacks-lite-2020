c4b6014799385fe08635e48a7004cabe9949250e
ShawnMeister <shawn.mountenay@gmail.com>
2020-10-31 17:22:37 -0700

    testing antdesign

--- src/front-end/src/App.vue
+++ src/front-end/src/App.vue
@@ -3,16 +3,29 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <button v-on:click="getLocation">Get my location</button>
+    <a-date-picker :defaultValue="moment()" />
+    <Layout>
+      <Header> test </Header>
+    </Layout>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import moment from "moment";
+import { version } from "ant-design-vue";
 
 export default {
   name: "App",
+
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      moment,
+      version,
+    };
   },
   methods: {
     getLocation() {
@@ -34,3 +47,5 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
