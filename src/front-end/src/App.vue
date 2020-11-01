224eca3fceb84466c90e158fedb619253756d0ea
ShawnMeister <shawn.mountenay@gmail.com>
2020-10-31 18:23:07 -0700

    navbar added and a bit of layout..

--- src/front-end/src/App.vue
+++ src/front-end/src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <button v-on:click="getLocation">Get my location</button>
-    <a-date-picker :defaultValue="moment()" />
-    <Layout>
-      <Header> test </Header>
-    </Layout>
+    <b-navbar toggleable="lg" type="dark" variant="secondary">
+      <b-navbar-brand href="#"
+        ><img
+          src="./assets/compass1.png"
+          style="width: 70px; height: auto"
+          alt="compass"
+        />
+        <span class="mt-auto" style="font-size: xx-large">
+          GeoChat</span
+        ></b-navbar-brand
+      >
+      <img class="ml-auto" src="./assets/github_iconSmall.png" />
+    </b-navbar>
+
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />-->
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col cols="2">
+          <ChatRoomNav msg="Chat Room Navigation" />
+
+          <button v-on:click="getLocation">Get my location</button>
+          <a-date-picker :defaultValue="moment()"
+        /></b-col>
+        <b-col cols="10"></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ChatRoomNav from "./components/ChatRoomNav.vue";
 import moment from "moment";
 import { version } from "ant-design-vue";
 
@@ -19,7 +38,7 @@ export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    ChatRoomNav,
   },
   data() {
     return {
@@ -38,13 +57,13 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
 
--- src/front-end/src/main.js
+++ src/front-end/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Antd from "ant-design-vue";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 import "ant-design-vue/dist/antd.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(Antd);
 
 Vue.config.productionTip = false
