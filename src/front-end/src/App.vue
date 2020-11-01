f6e55f03cdb34fe4d1bc2fdb64ed2dafb4ab9424
ShawnMeister <shawn.mountenay@gmail.com>
2020-10-31 20:02:14 -0700

    fixed merge issues

--- src/front-end/src/App.vue
+++ src/front-end/src/App.vue
@@ -44,7 +44,6 @@ import ChatRoom from "./components/ChatRoom.vue";
 import moment from "moment";
 import { version } from "ant-design-vue";
 
-import HelloWorld from "./components/HelloWorld.vue";
 import * as axios from "axios";
 
 export default {
